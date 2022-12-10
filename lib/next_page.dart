4148dfbcb9ca1fae77c5a7fc6f26a14ae75cbbcf
ryosuke-horie <ryosuke03140@gmail.com>
2022-12-10 16:46:14 +0900

    nextページのUIを作成

--- lib/next_page.dart
+++ lib/next_page.dart
@@ -7,6 +7,31 @@ class NextPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: const [
+                SizedBox(width: 30, child: Text('ID')),
+                Text(': 毎回同じIDを表示'),
+              ],
+            ),
+
+            // 余白
+            SizedBox(height: 20,),
+
+            Row(
+              children: const [
+                SizedBox(width: 30, child: Text('PW')),
+                Text(': 毎回同じパスワードを表示'),
+              ],
+            ),
+            Text(''),
+          ],
+        ),
+      ),
     );
   }
 }
